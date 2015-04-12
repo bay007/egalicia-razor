618be1b7d51627b707d3c86a1a7944c0f0452ad6
bay007 <tnt_code@hotmail.com>
2015-04-12 03:24:41 -0500

    Corrigiendo faltas de ortografía

--- asesoria.php
+++ asesoria.php
@@ -5,7 +5,7 @@ echo '<div id="contenido" class="col-md-8">
 	<div class="row" id="rowES">
 		<div class="jumbotron">
 		  <div class="container">
-			<p>En Razor Robot Design estamos para ayudarte en tu proyecto, sugiriendo y dando ideas para que obtengas la mejro eificiencia de tus piezas con el menor gasto posible respetando siempre tu diseño propio.</p>
+			<p>En Razor Robot Design estamos para ayudarte en tu proyecto, sugiriendo y dando ideas para que obtengas la mejor eificiencia de tus piezas con el menor gasto posible respetando siempre tu diseño propio.</p>
 			<p><h5> Envíanos archivos y platícanos lo que buscas.</h5></p>
 		  </div>
 		</div>
